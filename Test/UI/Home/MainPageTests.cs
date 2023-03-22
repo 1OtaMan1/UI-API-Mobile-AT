46725de15ea89d5b2ff4bd331afc9cc89309515c
1OtaMan1 <oleksandr.makar.ol@gmail.com>
2023-03-22 16:41:42 +0200

    Added Appium structure

--- Test/UI/Home/MainPageTests.cs
+++ Test/UI/Home/MainPageTests.cs
@@ -1,7 +1,7 @@
-﻿using Atata;
+﻿using System.Threading;
+using Atata;
 using Core.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
 using UI.Atata.Extensions;
 using UI.Business.Home;
 using static Core.Constants.TestCategories;
@@ -19,18 +19,28 @@ public class MainPageTests : BaseUiTest
 
         var mainPage = GoWithRetry.To<MainPage>(url: Url.BaseAppUrl);
         mainPage.Videos[0].Wait(Until.Visible);
+
+        Thread.Sleep(2000);
+
         mainPage.HeaderElements.SearchInput.Wait(Until.Visible);
 
         //  Check video search
         mainPage.HeaderElements.SearchInput.Set(expectedVideoTitle);
         mainPage.HeaderElements.SearchInput.Should.WithRetry.Contain(expectedVideoTitle);
+
+        Thread.Sleep(2000);
+
         var searchResultPage = mainPage.HeaderElements.SearchIcon.ClickAndGo();
 
         searchResultPage.GetVideo(expectedVideoTitle).Wait(Until.Visible);
 
+        Thread.Sleep(2000);
+
         //  Check opened video
         var playerPage = searchResultPage.GetVideo(expectedVideoTitle).Title.ClickAndGo();
         playerPage.VideoTitle.Wait(Until.Visible);
+
+        Thread.Sleep(2000);
 
         Assert.IsTrue(playerPage.VideoTitle.Content.Value.Contains(expectedVideoTitle), "Expected video should be opened");
     }
